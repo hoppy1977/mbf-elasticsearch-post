a6eb33ded038d9f00c90257e8a1a51622e4fc228
Shane Hopcroft <shanehopcroft@hotmail.com>
2017-10-24 11:11:43 +1100

    Inserting code to post data to elastic-search into Lambdas function.

--- post.js
+++ post.js
@@ -1,23 +1,39 @@
 var request = require('request');
 
-request.post(
-    'https://search-mbf-ti72ai7ynsdbshxifgym4z7mhy.ap-southeast-2.es.amazonaws.com/mbf/temperature',
-    {   
-        json:
-        {
-            "TimeStamp":Date.now().toString(),
-            "beerTemperature":"999",
-            "fridgeTemperature":"888",
-            "roomTemperature":"888"
-        }
-    },
-    function (error, response, body) {
-        if (!error && response.statusCode == 201) {
-            console.log(body)
-        }
-        else
-        {
-            console.log("Error");
-        }
-    }
-);
+'use strict';
+
+console.log('Loading function');
+
+exports.handler = (event, context, callback) => {
+    event.Records.forEach((record) => {
+        console.log(record.eventID);
+        console.log(record.eventName);
+        console.log('DynamoDB Record: %j', record.dynamodb);
+
+// *******************
+        request.post(
+            'https://search-mbf-ti72ai7ynsdbshxifgym4z7mhy.ap-southeast-2.es.amazonaws.com/mbf/temperature',
+            {   
+                json:
+                {
+                    "TimeStamp":Date.now().toString(),
+                    "beerTemperature":"999",
+                    "fridgeTemperature":"888",
+                    "roomTemperature":"777"
+                }
+            },
+            function (error, response, body) {
+                if (!error && response.statusCode == 201) {
+                    console.log(body)
+                }
+                else
+                {
+                    console.log("Error");
+                }
+            }
+        );
+// *******************
+    });
+
+    callback(null, `Successfully processed ${event.Records.length} records.`);
+};
